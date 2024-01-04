e48b57af068373f61e7557df75724bbcf8b22333
Santosh Venkumahanti <ubuntu@intel-mlops.200403109731.us-region-1.idcservice.net>
2024-01-04 03:02:10 +0000

    Lab1 files updated as per course

--- MLOps_Professional/lab1/sample/maintenance.py
+++ MLOps_Professional/lab1/sample/maintenance.py
@@ -1,18 +1,23 @@
-# maitnenace test business logic
+# maintenance test business logic
 
-def test_maintenance(temperature:int):
-    """_summary_
+def test_maintenance(temperature: int, hydraulic_pressure: int):
+    """Tests if maintenance is needed based on temperature and hydraulic pressure readings.
 
     Parameters
     ----------
     temperature : int
-        test parameter for temperature sensor readings
+        Test parameter for temperature sensor readings.
+    hydraulic_pressure : int
+        Test parameter for hydraulic pressure readings.
 
     Returns
     -------
     str
-        'Approved' or 'Denied' based on temperature readings
+        'Needs Maintenance' or 'No Maintenance Required' based on readings.
     """
-    maintenance_status = 'Needs Maintenance' if temperature > 50 else 'No Maintenance Required'
+    if temperature > 50 or hydraulic_pressure > 2000:
+        maintenance_status = 'Needs Maintenance'
+    else:
+        maintenance_status = 'No Maintenance Required'
     
     return maintenance_status
--- MLOps_Professional/lab1/sample/serve.py
+++ MLOps_Professional/lab1/sample/serve.py
@@ -2,19 +2,19 @@ import uvicorn
 import logging
 import warnings
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from data_model import MaintenancePayload
 from maintenance import test_maintenance
 
 
-app = FastAPI()
+appli = FastAPI()
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore")
 
 
-@app.get("/ping")
+@appli.get("/ping")
 async def ping():
     """Ping server to determine status
 
@@ -25,11 +25,30 @@ async def ping():
     """
     return {"message":"Server is Running"}
 
-@app.post("/maintenance")
+@appli.post("/maintenance")
 async def predict(payload:MaintenancePayload):
     
-    maintenance_result = test_maintenance(payload.temperature)
+    maintenance_result = test_maintenance(payload.temperature, payload.hydraulic_pressure)
     return {"msg": "Completed Analysis", "Maintenance Status": maintenance_result}
 
+@appli.post("/supportbot")
+async def supportbot(request: dict):
+    """Endpoint for supportbot
+
+    Parameters
+    ----------
+    request : dict
+        A request dictionary containing a 'text' key.
+
+    Returns
+    -------
+    API response
+        Hard-coded response for maintenance
+    """
+    if request.get("text") == "help":
+        return {"response": "Bring the harvester in for maintenance"}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid request") 
+
 if __name__ == "__main__":
-    uvicorn.run("serve:app", host="0.0.0.0", port=5000, log_level="info")
+    uvicorn.run("serve:appli", host="0.0.0.0", port=5000, log_level="info")
